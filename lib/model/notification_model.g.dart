d6b36f8c1c2272ea1e86fab3e20afbcf7360c63f
Shivam <bulletspeed12345@gmail.com>
2025-12-17 11:33:44 +0530

    Remain: Error solved storing Notification. but not storing while app was closed

---
+++ lib/model/notification_model.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'notification_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class NotificationModelAdapter extends TypeAdapter<NotificationModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  NotificationModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return NotificationModel(
+      heading: fields[0] as String,
+      content: fields[1] as String?,
+      bigPicture: fields[2] as String?,
+      receivedAt: fields[4] as DateTime,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, NotificationModel obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.heading)
+      ..writeByte(1)
+      ..write(obj.content)
+      ..writeByte(2)
+      ..write(obj.bigPicture)
+      ..writeByte(4)
+      ..write(obj.receivedAt);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is NotificationModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
